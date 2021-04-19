3735da1c3e017cc1dafdb7733a07d88a01614f8f
Jordan Gilmartin <jordan.gilmartin@yahoo.com>
2021-04-19 14:44:17 -0400

    updating app.js

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -37,11 +37,11 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(healthData, d => d.poverty)])
+      .domain([8, d3.max(healthData, d => d.poverty)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(healthData, d => d.smokes)])
+      .domain([5, d3.max(healthData, d => d.smokes)])
       .range([height, 0]);
 
     // Step 3: Create axis functions
@@ -60,15 +60,29 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
 
       // Step 5: Create Circles
     // ==============================
-    var circlesGroup = chartGroup.selectAll("circle")
+    chartGroup.selectAll("circle")
     .data(healthData)
     .enter()
     .append("circle")
     .attr("cx", d => xLinearScale(d.poverty))
     .attr("cy", d => yLinearScale(d.smokes))
-    .attr("r", 5)
-    .attr("fill", "pink")
-    .attr("opacity", ".5");
+    .attr("r", 20)
+    .attr("class", "stateCircle");
+    // .attr("fill", "pink")
+    // .attr("opacity", ".5");
+
+    chartGroup.selectAll("circle")
+    .data(healthData)
+    .enter()
+    .append("text")
+    .text(d => d.abbr)
+    .attr("dx", d => xLinearScale(d.poverty))
+    .attr("dy", d => yLinearScale(d.smokes))
+    .attr("font-size", 5)
+    .attr("class", "stateText");
+
+
+
 
     // Create axes labels
     chartGroup.append("text")
@@ -76,12 +90,12 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
       .attr("y", 0 - margin.left + 40)
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
-      .attr("class", "axisText")
-      .text("Smokes (%)");
+      .attr("class", "aText")
+      .text("Smokers (%)");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-      .attr("class", "axisText")
+      .attr("class", "aText")
       .text("Poverty (%)");
 
 
